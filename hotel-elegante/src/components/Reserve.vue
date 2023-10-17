65bee1951cacf57e11cbd1541e7bc8942ea271f6
Daviduo <davide0400@gmail.com>
2023-10-18 00:14:12 +0200

    carosello corretto

--- hotel-elegante/src/components/Reserve.vue
+++ hotel-elegante/src/components/Reserve.vue
@@ -4,6 +4,7 @@ import NavigationComponent from './NavigationComponent.vue';
 import { useRoute } from 'vue-router';
 import Tooltip from 'primevue/tooltip';
 import axios from 'axios';
+import Carousel from 'primevue/carousel';
 
 const route = useRoute();
 const roomsData = ref([]);
@@ -29,6 +30,27 @@ const roomTypes = computed(() => {
         };
     });
 });
+
+const roomImages = {
+    suite: [
+        'https://i.ibb.co/pW71Ff9/suite1.png',
+        'https://i.ibb.co/fDRmKhf/suite2.png',
+        'https://i.ibb.co/Sr6Bw4k/suite3.png',
+        'https://i.ibb.co/p3btt33/suite4.png'
+    ],
+    singola: [
+        'https://i.ibb.co/98PxRQh/single1.png',
+        'https://i.ibb.co/nBWxqcT/single2.png',
+        'https://i.ibb.co/CV8678j/single3.png',
+        'https://i.ibb.co/18gxDXz/single4.png'
+    ],
+    doppia: [
+        'https://i.ibb.co/D94ML6s/double1.png',
+        'https://i.ibb.co/w6qQNH7/double2.png',
+        'https://i.ibb.co/HnbkStF/double3.png',
+        'https://i.ibb.co/YcMx0bM/double4.png'
+    ]
+};
 </script>
 
 <template>
@@ -104,9 +126,17 @@ const roomTypes = computed(() => {
         </template>
     </NavigationComponent>
 
+    <div class="title">Reserve a room</div>
+
     <div class="rooms-info">
         <div v-for="roomType in roomTypes" :key="roomType.type" class="room-type-info">
             <h3>{{ roomType.type.charAt(0).toUpperCase() + roomType.type.slice(1) }}</h3>
+            <!-- Applica la classe custom-carousel al componente Carousel -->
+            <Carousel :value="roomImages[roomType.type]" :numVisible="1" :numScroll="1" :circular="true" class="custom-carousel">
+                <template #item="slotProps">
+                    <img :src="slotProps.data" alt="Room Image" class="room-image" />
+                </template>
+            </Carousel>
             <p>Prezzo: €{{ roomType.price }}</p>
             <p>Camere disponibili: {{ roomType.available }}</p>
         </div>
@@ -116,8 +146,28 @@ const roomTypes = computed(() => {
 
 <style scoped>
 
-.room-type-info {
-    margin-bottom: 20px;
+.rooms-info {
+    margin-top: 50px;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
 }
+
+.room-type-info{
+    width: calc(100% / 4);
+
+}
+
+.custom-carousel {
+    width: 300px;  /* Modifica a seconda delle tue esigenze */
+    height: 200px; /* Modifica a seconda delle tue esigenze */
+}
+
+.room-image {
+    max-width: 100%;
+    height: auto;
+}
+
+
 
 </style>
