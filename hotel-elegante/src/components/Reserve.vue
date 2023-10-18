46dd8beaecaacbd9099128ad68b737b9bb0aefea
Daviduo <davide0400@gmail.com>
2023-10-18 20:55:12 +0200

    added reserve loading and "sort-of" completed the page

--- hotel-elegante/src/components/Reserve.vue
+++ hotel-elegante/src/components/Reserve.vue
@@ -6,6 +6,22 @@ import Tooltip from 'primevue/tooltip';
 import axios from 'axios';
 import Carousel from 'primevue/carousel';
 
+
+const isLoading = ref(true); // inizialmente impostato su true
+
+onMounted(async () => {
+    try {
+        // await new Promise(resolve => setTimeout(resolve, 5000));  // aspetta 5 secondi
+        const { data } = await axios.get('http://localhost:8000/api/rooms');
+        roomsData.value = data;
+    } catch (error) {
+        console.error("Errore nel recupero dei dati delle camere:", error);
+    } finally {
+        isLoading.value = false; // imposta il caricamento su false sia che ci sia un successo che un errore
+    }
+});
+
+
 const route = useRoute();
 const roomsData = ref([]);
 
@@ -19,7 +35,7 @@ onMounted(async () => {
 });
 
 const roomTypes = computed(() => {
-    const types = ['suite', 'singola', 'doppia'];
+    const types = ['suite', 'single', 'double'];
     return types.map(type => {
         const roomsOfType = roomsData.value.filter(room => room.type === type);
         const availableRooms = roomsOfType.filter(room => room.is_available === 1).length;
@@ -38,13 +54,13 @@ const roomImages = {
         'https://i.ibb.co/Sr6Bw4k/suite3.png',
         'https://i.ibb.co/p3btt33/suite4.png'
     ],
-    singola: [
+    single: [
         'https://i.ibb.co/98PxRQh/single1.png',
         'https://i.ibb.co/nBWxqcT/single2.png',
         'https://i.ibb.co/CV8678j/single3.png',
         'https://i.ibb.co/18gxDXz/single4.png'
     ],
-    doppia: [
+    double: [
         'https://i.ibb.co/D94ML6s/double1.png',
         'https://i.ibb.co/w6qQNH7/double2.png',
         'https://i.ibb.co/HnbkStF/double3.png',
@@ -52,10 +68,50 @@ const roomImages = {
     ]
 };
 
+const roomDetails = {
+    single: {
+        description:
+            "The main room area with a comfortable bed, offering a glimpse of the city through the window. An illustration of the ensuite bathroom, highlighting its modern fixtures and luxury feel. A dedicated workspace within the room, equipped with all essentials for a productive stay. A cozy lounge area in the room, perfect for relaxation and reading.",
+        services: [
+            { icon: "fa-solid fa-wifi", description: "Free WiFi" },
+            { icon: "fa-solid fa-tv", description: "TV" },
+            { icon: "fa-solid fa-bell-concierge", description: "Room Service" },
+            { icon: "fa-solid fa-fan", description: "Air Conditioning" },
+        ],
+    },
+    double: {
+        description:
+            "The main room area with twin beds, arranged to provide maximum comfort while ensuring a spacious feel. An illustration of the ensuite bathroom, designed with modern fixtures and twin sinks for the convenience of guests sharing the room. A lounge area within the room, perfect for guests to relax, watch TV, or enjoy a cup of tea. A dedicated workspace designed to accommodate two guests, ensuring both have ample space and amenities to work comfortably.",
+        services: [
+            { icon: "fa-solid fa-wifi", description: "Free WiFi" },
+            { icon: "fa-solid fa-tv", description: "TV" },
+            { icon: "fa-solid fa-bell-concierge", description: "Room Service" },
+            { icon: "fa-solid fa-fan", description: "Air Conditioning" },
+            { icon: "fa-solid fa-mug-hot", description: "Tea and Coffee Making Facilities" },
+            { icon: "fa-solid fa-panorama", description: "City View" }
+        ],
+    },
+    suite: {
+        description:
+            "The Bedroom area is designed for ultimate comfort and luxury, with a spacious king-sized bed and a seating area. The Bathroom, featuring a jacuzzi, rain shower, and high-end amenities to provide a spa-like experience. A living area furnished with modern decor and designed for relaxation and entertainment. The suite's private balcony, offering a serene spot to relax and enjoy the views, be it a romantic dinner or a peaceful morning coffee session.",
+        services: [
+            { icon: "fa-solid fa-wifi", description: "Free WiFi" },
+            { icon: "fa-solid fa-tv", description: "TV" },
+            { icon: "fa-solid fa-bell-concierge", description: "Room Service" },
+            { icon: "fa-solid fa-mug-hot", description: "Tea and Coffee Making Facilities" },
+            { icon: "fa-solid fa-hot-tub-person", description: "Jacuzzi" },
+            { icon: "fa-solid fa-shower", description: "Rain Shower" },
+            { icon: "fa-solid fa-fan", description: "Air Conditioning" },
+            { icon: "fa-solid fa-door-open", description: "Private Balcony" },
+            { icon: "fa-solid fa-gifts", description: "Luxury Amenities" },
+        ],
+    }
+};
+
 const hoveredStatus = ref({
     suite: false,
-    singola: false,
-    doppia: false
+    single: false,
+    double: false
 });
 
 
@@ -144,13 +200,16 @@ const onLeave = (roomType) => {
         </template>
     </NavigationComponent>
 
+    <div v-if="isLoading" class="loading-overlay">
+        <i class="pi pi-spin pi-spinner" style="font-size: 2rem; color: #b97533"></i>
+        <p> We are checking the Rooms... </p>
+    </div>
+
     <div class="title">Reserve a room</div>
 
     <div class="wrapper">
         <div v-for="roomType in roomTypes" :key="roomType.type" class="rooms-info">
-            <div class="room-type-info"
-                v-bind:class="{ 'hovered': hoveredStatus[roomType.type] }"
-            >
+            <div class="room-type-info" v-bind:class="{ 'hovered': hoveredStatus[roomType.type] }">
                 <div class="title" style="font-size: 22px; padding-bottom: 10px;">{{ roomType.type.charAt(0).toUpperCase() + roomType.type.slice(1) }}</div>
                 <!-- Applica la classe custom-carousel al componente Carousel -->
                 <Carousel :value="roomImages[roomType.type]" :numVisible="1" :numScroll="1" :circular="true" class="custom-carousel">
@@ -159,15 +218,36 @@ const onLeave = (roomType) => {
                     </template>
                 </Carousel>
                 <div class="infos">
-                    <div class="info-box"><span class="symbol"> <i class="pi pi-dollar"></i></span>{{ roomType.price }}</div>
-                    <div class="info-box"><span class="symbol"> <i class="pi pi-building"></i></span> {{ roomType.available }}</div>
+                    <div class="info-box">
+                        <span class="symbol"> 
+                            <i class="pi pi-dollar"></i>
+                        </span>{{ roomType.price }}
+                    </div>
+                    <div class="info-box">
+                        <span class="symbol"> 
+                            <i class="pi pi-building"></i>
+                        </span>{{ roomType.available }}
+                    </div>
                 </div>
+
+            </div>
+
+            <div class="services" v-if="roomDetails[roomType.type] && roomDetails[roomType.type].services">
+                <div class="service" v-for="service in roomDetails[roomType.type].services" :key="service.description" v-bind:class="{ 'hovered': hoveredStatus[roomType.type] }">
+                    <i :class="service.icon" v-tooltip.bottom="service.description"></i>
+                </div>
+            </div>
+            <!-- Fine delle icone dei servizi -->
+
+            <div class="description" v-if="roomDetails[roomType.type]">
+                <p>{{ roomDetails[roomType.type].description }}</p>
+                
             </div>
             <div class="reserve"
                 @mouseover="onHover(roomType.type)" 
                 @mouseleave="onLeave(roomType.type)"
             >
-                I choose this!
+                i choose {{roomType.type}}
             </div>
         </div>
     </div>
@@ -177,6 +257,36 @@ const onLeave = (roomType) => {
 
 <style scoped>
 
+.loading-overlay {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(255, 255, 255, 0.9); /* sfondo bianco con leggera trasparenza */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+}
+
+.loader {
+    border: 16px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 16px solid #3498db;
+    width: 80px;
+    height: 80px;
+    animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+
 .wrapper{
     margin-top: 50px;
     width: 100vw;
@@ -184,8 +294,6 @@ const onLeave = (roomType) => {
     justify-content: center;
     flex-wrap: wrap;
     gap: 20px;
-    margin-left: 30px;
-    margin-right: 30px;
 }
 
 .room-type-info{
@@ -241,6 +349,7 @@ const onLeave = (roomType) => {
     display: flex;
     justify-content: center;
     padding: 10px 7px;
+    text-transform: uppercase;
 }
 
 .reserve:hover{
@@ -251,6 +360,37 @@ const onLeave = (roomType) => {
 
 .room-type-info.hovered {
     border: 3px solid #b97533;
+}
+
+.rooms-info{
+    width: 380px;
+}
+
+.services{
+    margin-top: 5px;
+    display: flex;
+    width: 380px;
+    height: 50px;
+    gap: 10px;
+    justify-content: center;
+    align-items: center;
+}
+
+.service{
+    border: 1px solid grey;
+    border-radius: 8px;
+    padding: 5px 5px;
+}
+
+.service.hovered{
+    border: 1px solid #b97533;
+    background-color: #b97533;
+    color: #fff;
+}
+
+.description{
+    width: 380px;
+    height: 210px;
 }
 
 </style>
