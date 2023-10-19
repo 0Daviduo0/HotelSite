72ca5e71938e48afe5b9f05da58c2a8ff3c7f1ab
Daviduo <davide0400@gmail.com>
2023-10-19 15:10:36 +0200

    added brantree ui in frontend

--- hotel-elegante/src/components/ReserveComponents/Payment.vue
+++ hotel-elegante/src/components/ReserveComponents/Payment.vue
@@ -1,13 +1,65 @@
 <template>
-
-    this is the payment section
-
+    <!-- ... altri elementi ... -->
+    <div id="dropin-container"></div>
+    <button id="submit-button">Paga ora</button>
+    <!-- ... altri elementi ... -->
 </template>
 
 <script setup>
-import { defineEmits } from 'vue';
+import { ref, onMounted } from 'vue';
 
-const emits = defineEmits(['detailsUpdate']);
+const instance = ref(null);
 
-// ... il resto del tuo codice setup ...
+onMounted(async () => {
+    await initializeBraintree();
+});
+
+async function initializeBraintree() {
+    // Ottieni il client token dal tuo backend
+    const response = await fetch('http://localhost:8000/api/braintree/token');
+    const data = await response.json();
+    const clientToken = data.token;
+
+    // Inizializza il drop-in UI
+    braintree.dropin.create({
+        authorization: clientToken,
+        container: '#dropin-container'
+    }, (createErr, braintreeInstance) => {
+        if (createErr) {
+            console.error('Errore nella creazione del drop-in:', createErr);
+            return;
+        }
+        instance.value = braintreeInstance;
+    });
+}
+
+async function processPayment() {
+    instance.value.requestPaymentMethod(async (requestPaymentMethodErr, payload) => {
+        if (requestPaymentMethodErr) {
+            console.error('Errore nella richiesta del metodo di pagamento:', requestPaymentMethodErr);
+            return;
+        }
+
+        // Invia il nonce al tuo server per effettuare il pagamento
+        const response = await fetch('http://localhost:8000/api/braintree/payment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nonce: payload.nonce,
+                // Imposta l'importo del pagamento. Questo dovrebbe provenire dal tuo frontend o backend.
+                amount: '10.00' 
+            })
+        });
+
+        const data = await response.json();
+        if (data.success) {
+            alert('Pagamento effettuato con successo!');
+        } else {
+            alert('Errore nel pagamento:', data.message);
+        }
+    });
+}
+
 </script>
